d0027abf908d42342fe37ac1d52354c3d8617659
Radu Siderias <radusiderias@yahoo.com>
2023-11-05 14:34:36 +0200

    added buttons in home activity

--- app/src/main/java/com/example/roadmaster/HomeActivity.kt
+++ app/src/main/java/com/example/roadmaster/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.roadmaster
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.RelativeLayout
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val toChangePasswordButton: ImageButton = findViewById(R.id.changePasswordHomeButton)
+        toChangePasswordButton.setOnClickListener{
+            startActivity(Intent(this, ChangePasswordActivity::class.java))
+        }
     }
 }
