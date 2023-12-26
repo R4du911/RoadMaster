69cea8e1e720064d54147a6b67f59dae2aef00bd
Radu Siderias <radusiderias@yahoo.com>
2023-12-26 13:17:43 +0200

    tips feature working

--- app/src/main/java/com/example/roadmaster/activities/DrivingTipsInfoActivity.kt
+++ app/src/main/java/com/example/roadmaster/activities/DrivingTipsInfoActivity.kt
@@ -4,15 +4,55 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import com.example.roadmaster.R
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.readText
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.isSuccess
+import kotlinx.coroutines.launch
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.json.JSONObject
 
 class DrivingTipsInfoActivity : AppCompatActivity() {
+
+    private val httpClient = HttpClient(Android) {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(Json {
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_driving_tips_info)
 
         val myIntent = intent
         val userData = myIntent.getStringExtra("user")
+
+        val textViewInfo: TextView = findViewById(R.id.tipsInfoText)
+
+        lifecycleScope.launch {
+            val infoList : List<String>? = tipsGet()
+            val stringBuilder = StringBuilder()
+
+            if (infoList != null) {
+                for (info in infoList) {
+                    stringBuilder.append("• ").append(info).append("\n").append("\n")
+                }
+            }
+
+            textViewInfo.text = stringBuilder.toString()
+        }
 
         val toChooseInfoActivityButton: ImageButton = findViewById(R.id.backToChooseInfoCategoryFromDrivingTips)
         toChooseInfoActivityButton.setOnClickListener{
@@ -21,5 +61,28 @@ class DrivingTipsInfoActivity : AppCompatActivity() {
 
             startActivity(chooseInfoActivity)
         }
+    }
+
+    private suspend fun tipsGet(): List<String>? {
+        try {
+            val response: HttpResponse = httpClient.get("http://10.0.2.2:8000/api/tips") {
+                contentType(ContentType.Application.Json)
+            }
+
+            if (response.status.isSuccess()) {
+                val json = JSONObject(response.readText())
+                val userData = json.getJSONObject("data")
+                val tipsJSON = userData.getString("tips")
+
+                return Json.decodeFromString(tipsJSON)
+
+            } else {
+                println("Loading tips failed. Status: ${response.status}")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return null
     }
 }
