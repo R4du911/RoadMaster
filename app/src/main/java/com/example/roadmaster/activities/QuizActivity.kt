2685f07f33c277ba22c1b6b1b27f9938fba2cd0d
petrid <dragospetri@yahoo.com>
2024-01-06 17:49:15 +0200

    implement 2 finish conditions without implementing consequences

--- app/src/main/java/com/example/roadmaster/activities/QuizActivity.kt
+++ app/src/main/java/com/example/roadmaster/activities/QuizActivity.kt
@@ -22,7 +22,9 @@ import io.ktor.http.isSuccess
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import org.json.JSONObject
-import java.util.Vector
+
+
+private const val maxQuestions: Int = 26;
 
 class QuizActivity : AppCompatActivity() {
 
@@ -34,20 +36,23 @@ class QuizActivity : AppCompatActivity() {
         }
     }
 
+    private var questionCounter: Int = 0;
+    private var questionIds: MutableList<Int> = mutableListOf();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
 
         val timeText: TextView = findViewById(R.id.timer)
 
-        val timer = object : CountDownTimer(20000, 1000) {
+        val timer = object : CountDownTimer(1_800_000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 timeText.text =
                     buildString {
                         append("Timp ramas ")
                         append(((millisUntilFinished / 1000) / 60))
                         append(" : ")
-                        append((millisUntilFinished / 1000))
+                        append((millisUntilFinished / 1000) % 60)
                     }
             }
 
@@ -57,41 +62,80 @@ class QuizActivity : AppCompatActivity() {
         }
         timer.start()
 
+        questionCounter = 0
         val nextQuestion: Button = findViewById(R.id.next_question)
         val category = intent.getStringExtra("category");
 
         nextQuestion.setOnClickListener {
             lifecycleScope.launch { getQuestion(category) }
+            if (questionCounter == 0) {
+                questionCounter++
+                val q_counter: TextView = findViewById(R.id.question_counter)
+                q_counter.text = buildString {
+                    append(questionCounter)
+                    append("/")
+                    append(maxQuestions)
+                }
+            }
+            else if (questionCounter == maxQuestions)
+            {
+                val q_counter: TextView = findViewById(R.id.question_counter)
+                q_counter.text = buildString {
+                    append("DOne")
+                }
+            }
+            else
+            {
+                questionCounter++
+                val q_counter: TextView = findViewById(R.id.question_counter)
+                q_counter.text = buildString {
+                    append(questionCounter)
+                    append("/")
+                    append(maxQuestions)
+                }
+            }
         }
         nextQuestion.performClick();
     }
 
-    private suspend fun getQuestion(category : String?) {
+    private suspend fun getQuestion(category: String?) {
         try {
-            val response: HttpResponse = httpClient.post("http://10.0.2.2:8000/api/question")
-            {
-                contentType(ContentType.Application.Json)
-                body = GetQuestionRequestDTO(category)
-            }
-
-            if (response.status.isSuccess()) {
-                val json = JSONObject(response.readText())
-                val userData = json.getJSONObject("data")
-
-                val question: TextView = findViewById(R.id.question)
-                question.text = userData.getString("text")
-
-                val answers = userData.getJSONArray("answers");
-                val checkBoxes :  Array<CheckBox> = arrayOf(
-                    findViewById(R.id.answer1),
-                    findViewById(R.id.answer2),
-                    findViewById(R.id.answer3),
-                    findViewById(R.id.answer4))
-
-                for (i in 0 until answers.length())
+            while (true) {
+                val response: HttpResponse = httpClient.post("http://10.0.2.2:8000/api/question")
                 {
-                    val answer = answers.getJSONArray(i)
-                    checkBoxes[i].text = answer.getString(0);
+                    contentType(ContentType.Application.Json)
+                    body = GetQuestionRequestDTO(category)
+                }
+
+                if  (response.status.isSuccess()) {
+
+                    val json = JSONObject(response.readText())
+                    val questionData = json.getJSONObject("data")
+
+                    val questionId = questionData.getInt("id")
+                    if (questionId in questionIds) {
+                        continue
+                    }
+
+                    questionIds.add(questionId)
+
+                    val question: TextView = findViewById(R.id.question)
+                    question.text = questionData.getString("text")
+
+                    val answers = questionData.getJSONArray("answers");
+                    val checkBoxes: Array<CheckBox> = arrayOf(
+                        findViewById(R.id.answer1),
+                        findViewById(R.id.answer2),
+                        findViewById(R.id.answer3),
+                        findViewById(R.id.answer4)
+                    )
+
+                    for (i in 0 until answers.length()) {
+                        val answer = answers.getJSONArray(i)
+                        checkBoxes[i].text = answer.getString(0);
+                    }
+
+                    break
                 }
             }
         } catch (e: Exception) {
